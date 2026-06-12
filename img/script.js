e24e76b0546e8d6a626c7b96aa3af6f5973e6822
luizpicolotto <luiz.picolotto@escola.pr.gov.br>
2026-06-12 09:09:38 -0300

    Update script.js

--- img/script.js
+++ img/script.js
@@ -1,3 +1,18 @@
-function toggleCard(card) {
-    card.classList.toggle('active');
+function abrirCard(card){
+    card.classList.toggle("ativo");
+}
+
+function mostrarDica(){
+
+    const dicas = [
+        "💧 Economize água utilizando irrigação por gotejamento.",
+        "🌱 Produza adubo orgânico através da compostagem.",
+        "🐞 Utilize controle biológico para reduzir pesticidas.",
+        "☀️ Invista em energia solar para diminuir custos.",
+        "🍃 Preserve a vegetação nativa ao redor da plantação."
+    ];
+
+    const sorteio = Math.floor(Math.random() * dicas.length);
+
+    document.getElementById("resultado").innerHTML = dicas[sorteio];
 }
